8f1f5a59b43dd9e0e303a2b1d77a3fac3855f93f
xodnjs0876 <xodnjs.kim@wavle.io>
2023-08-07 17:39:46 +0900

    공지사항 리스트 정보

---
+++ src/components/C0301/Container/AnnounceList/ListInfo/listInfo.js
@@ -0,0 +1,34 @@
+const notisInfo = [
+    {
+        id: 3,
+        title: '[공고][서울특별시] 「외식기업 판로개척 인턴십」 참여자 4차 모집 공고',
+        comments: 10,
+        attach: true,
+        writer: 'KFIRI',
+        date: '2023.01.01',
+        viewer: 99,
+        likes: 88, 
+    },
+    {
+        id: 2,
+        title: '[공고][서울특별시] 「외식기업 판로개척 인턴십」 협력사업장 모집 공고',
+        comments: 1,
+        attach: false,
+        writer: 'KFIRI',
+        date: '2023.01.01',
+        viewer: 88,
+        likes: 88, 
+    },
+    {
+        id: 1,
+        title: '[공고][서울특별시] 「외식기업 판로개척 인턴십」 참여자 2차 모집 공고',
+        comments: 99,
+        attach: true,
+        writer: 'KFIRI',
+        date: '2023.01.01',
+        viewer: 88,
+        likes: 81, 
+    }
+];
+
+export { notisInfo };
