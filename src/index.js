010e3b468e34d22ab8352407092c07f20a5c9c5b
xodnjs0876 <taewon.kim@wavle.io>
2023-08-14 13:36:58 +0900

    index 파일

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import C0301 from './C0301';
-import C0302 from './C0302';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <C0302/>
+    <C0301/>
   </React.StrictMode>
 );
 reportWebVitals();
