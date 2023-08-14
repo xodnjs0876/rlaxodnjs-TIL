6cef1b868cebb845149acf0ccffe0c181f4434e9
user.name <taewon.kim@wavle.io>
2023-08-14 11:07:11 +0900

    전체 파일 리스트 출력  Please enter the commit message for your changes. Lines starting

--- src/components/C0301/Container/AnnounceList/ListInfo/listInfo.js
+++ src/components/C0301/Container/AnnounceList/ListInfo/listInfo.js
@@ -1,12 +1,12 @@
 const notisInfo = [
     {
         id: 3,
-        title: '[공고][서울특별시] 「외식기업 판로개척 인턴십」 참여자 4차 모집 공고',
+        title: '[KRBI] 2023년 1분기 외식산업경기전망지수',
         comments: 10,
         attach: true,
         writer: 'KFIRI',
         date: '2023.01.01',
-        viewer: 99,
+        viewer: 88,
         likes: 88, 
     },
     {
@@ -22,7 +22,7 @@ const notisInfo = [
     {
         id: 1,
         title: '[공고][서울특별시] 「외식기업 판로개척 인턴십」 참여자 2차 모집 공고',
-        comments: 99,
+        comments: 99,   
         attach: true,
         writer: 'KFIRI',
         date: '2023.01.01',
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import C0301 from './C0301';
+import C0302 from './C0302';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <C0302/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
